a138ea950f2ebc0d7d2d6b757cee8bebb95a87a9
sungtae kim <sungtae@messagebird.com>
2019-04-19 08:53:14 +0200

    study stuffs

---
+++ cmd/the_go_programming_language/chapter_1/helloworld/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world")
+	fmt.Println("Hello, 세계")
+}
+
+// $ go run cmd/the_go_programming_language/chapter_1/helloworld/main.go
+// Hello, world
