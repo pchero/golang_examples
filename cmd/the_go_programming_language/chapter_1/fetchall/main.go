c4318fb359a66b01e5483e7b662dc04c66489d7c
sungtae kim <sungtae@messagebird.com>
2019-05-07 09:06:42 +0200

    study stuffs

---
+++ cmd/the_go_programming_language/chapter_1/fetchall/main.go
@@ -0,0 +1,50 @@
+// fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
+
+// $ go run main.go http://naver.com http://daum.net http://google.com
+// 0.10s   11801 http://google.com
+// 2.49s  176236 http://naver.com
+// 4.31s  225841 http://daum.net
+// 4.31s elapsed
