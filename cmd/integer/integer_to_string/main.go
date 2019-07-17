36cbab49f833341a413dd434f8535901cfbd29e5
sungtae kim <sungtae@messagebird.com>
2019-07-17 11:04:50 +0200

    Added stuffs

---
+++ cmd/integer/integer_to_string/main.go
@@ -0,0 +1,18 @@
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+
+	numString := strconv.Itoa(3)
+
+	// numString := String(3)
+	fmt.Println(numString)
+
+	return
+}
