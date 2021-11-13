7e06caa4ae4988ae5cd556caa5f2f13aa70a232d
Sungtae Kim <pchero21@gmail.com>
2021-11-14 02:35:27 +0900

    test

--- cmd/string/string_uuid/main.go
+++ cmd/string/string_uuid/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gofrs/uuid"
@@ -12,4 +13,25 @@ func main() {
 	fmt.Printf("uuid info: %s\n", id)
 
 	fmt.Printf("uuid info: %s\n", uuid.Must(uuid.NewV4()).String())
+
+	tmp := map[string]uuid.UUID{
+		"5a3a7138-44a3-11ec-8a23-93b5625893d3": uuid.FromStringOrNil("5a66aad2-44a3-11ec-87fd-53ddb650bd33"),
+	}
+	fmt.Printf("tmp: %v\n", tmp)
+
+	m, err := json.Marshal(tmp)
+	if err != nil {
+		fmt.Printf("Could not marshal the tmp. err: %v", err)
+	}
+	fmt.Printf("marshal: %v\n", m)
+
+	res := map[string]uuid.UUID{}
+	if err := json.Unmarshal(m, &res); err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	for k, v := range res {
+		fmt.Printf("res. key: %s, value: %s\n", k, v.String())
+	}
+
 }
